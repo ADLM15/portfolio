d66756ca1d5d111a7a4eb8bd273acfe61f6e4616
raoudache <raoud@email.com>
2026-03-17 12:58:07 +0100

    fix effet visuel

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -103,3 +103,25 @@ filterBtns.forEach(btn => {
 
     });
 });
+
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+    const windowHeight = window.innerHeight;
+
+    reveals.forEach(el => {
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
+
+document.addEventListener('mousemove', (e) => {
+    document.body.style.setProperty('--x', e.clientX + 'px');
+    document.body.style.setProperty('--y', e.clientY + 'px');
+});
